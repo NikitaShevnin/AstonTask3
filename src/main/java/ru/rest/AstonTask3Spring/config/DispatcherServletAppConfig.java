1a2d80000879891e83c268b167abc97c0dbca7cd
Администратор <nikita.shevnin2016@yandex.ru>
2024-10-29 10:04:45 +0300

    add comment to DispatcherServletAppConfig

--- src/main/java/ru/rest/AstonTask3Spring/config/DispatcherServletAppConfig.java
+++ src/main/java/ru/rest/AstonTask3Spring/config/DispatcherServletAppConfig.java
@@ -5,6 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Конфигурационный класс для настройки веб-приложения с использованием Spring MVC.
+ *
+ * <p>
+ * Данный класс включает аннотации:
+ * <ul>
+ *   <li>{@link Configuration} - обозначает, что этот класс является источником конфигурации Spring.</li>
+ *   <li>{@link EnableWebMvc} - включает поддержку Spring MVC.</li>
+ *   <li>{@link ComponentScan} - указывает Spring сканировать пакет "ru.rest.AstonTask3Spring"
+ *       для обнаружения компонентов, сервисов и контроллеров.</li>
+ * </ul>
+ * </p>
+ */
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.rest.AstonTask3Spring")
